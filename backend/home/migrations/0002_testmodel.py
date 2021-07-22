078297da0d4fb9753f6a3a12fb3ba909afbd396f
Crowdbotics <team@crowdbotics.com>
2021-07-22 16:54:09 +0000

    Update models

--- backend/home/migrations/0002_testmodel.py
+++ backend/home/migrations/0002_testmodel.py
@@ -8,15 +8,23 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('home', '0001_load_initial_data'),
+        ("home", "0001_load_initial_data"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='TestModel',
+            name="TestModel",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('username', models.TextField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("username", models.TextField()),
             ],
         ),
     ]
